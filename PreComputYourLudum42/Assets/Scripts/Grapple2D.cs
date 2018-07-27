f8c08510f8f4fba22a34e60ad14d5c4d97d43463
Reveillark <vlt31@hotmail.fr>
2018-07-27 23:48:03 +0200

    Visible Grappling hook
    The grappling hook is now visible, but I found a bug about it's horizontal use, which is caused by the way horizontal movements are done in the playerController script.

--- PreComputYourLudum42/Assets/Scripts/Grapple2D.cs
+++ PreComputYourLudum42/Assets/Scripts/Grapple2D.cs
@@ -1,12 +1,15 @@
 ﻿using UnityEngine;
 
+
 public class Grapple2D : MonoBehaviour {
 
     public float minDistance = 0.1f;
     public float force = 10f;
     [Tooltip("Things that the grapple can attach to")]
     public LayerMask grapplable;
+    public GameObject rope;
 
+    GameObject currentRope;
     Vector2 GrapplePoint;
     bool isGrappled;
     Rigidbody2D rb;
@@ -26,12 +29,14 @@ public class Grapple2D : MonoBehaviour {
         if (Input.GetButtonDown("Grapple"))
         {
             isGrappled = false;
+            Destroy(currentRope);
             RaycastHit2D[] hit = new RaycastHit2D[1];
             Vector3 target = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             if (coll.Raycast(target - this.transform.position, hit) > 0)
             {
                 GrapplePoint = hit[0].point;
                 isGrappled = true;
+                currentRope = Instantiate(rope);
             }
         }
 
@@ -40,7 +45,17 @@ public class Grapple2D : MonoBehaviour {
             if (Vector2.Distance(this.transform.position, GrapplePoint) > minDistance)
             {
                 Vector2 pos = this.transform.position;
+
+                Debug.Log((GrapplePoint - pos).normalized);
+
                 rb.AddForce((GrapplePoint - pos).normalized * force / Vector2.Distance(GrapplePoint,  pos), ForceMode2D.Impulse);
+                // Pourquoi latéralement ça foire ? Plus on s'approche et plus c'est dur de s'approcher, à courte distance
+                // De plus il semblerait que le grappin ne fonctionne pas du tout latértalement de manière générale
+                // Le problème viens de la gestion des déplacements, idiot !
+
+                LineRenderer line = currentRope.GetComponent<LineRenderer>();
+                Vector3[] plop = { this.transform.position, GrapplePoint };
+                line.SetPositions(plop);
             }
             else
             {
